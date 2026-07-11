58a136ac7b6be7c5cc64b913e818617f729d94fa
KB-Designs <kimanibrian030@gmail.com>
2026-07-11 23:57:07 +0300

    first commit

---
+++ custom_addons/electronics_store/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "Electronics Store",
+    "version": "19.0.1.0.0",
+    "summary": "Electronics Store Management",
+    "description": """
+Electronics Store ERP
+
+This module extends Odoo to manage
+electronics products, brands,
+warranty plans and future sales features.
+""",
+    "author": "Brian Kimani",
+    "website": "https://github.com/kimani-brian",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "product",
+        "stock",
+    ],
+    "data": [
+        
+    ],
+    "demo": [
+
+    ],
+    "installable": True,
+    "application": True,
+}
